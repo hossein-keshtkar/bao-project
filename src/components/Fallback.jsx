f7effe6282a56db88501a2a8ddac0c914fccf74b
hossein.k <120290053+boredSquidward@users.noreply.github.com>
2023-10-02 08:34:55 -0400

    Fallback created

---
+++ src/components/Fallback.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Fallback = () => {
+  return (
+    <div className="d-flex justify-content-center w-100 my-5">
+      <div className="spinner-border" role="status"></div>
+    </div>
+  );
+};
+
+export default Fallback;
